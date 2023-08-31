d355d7a3007c1f9619c85592f9f4cd5d82818467
osparking <jbpark03@gmail.com>
2023-08-31 12:55:59 +0900

    parrot injected by parrot setter of Person

--- src/main/java/jb/ssh/ch03_ex6/domain/Person.java
+++ src/main/java/jb/ssh/ch03_ex6/domain/Person.java
@@ -1,5 +1,6 @@
 package jb.ssh.ch03_ex6.domain;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import lombok.Getter;
@@ -9,12 +10,11 @@ import lombok.ToString;
 @Getter
 @ToString
 public class Person {
-	private String name;
+	private String name = "철수";
 	private Parrot parrot;
 
-	public Person(Parrot parrot) {
-		super();
-		this.name = "철수";
+	@Autowired
+	void setParrot(Parrot parrot) {
 		this.parrot = parrot;
 	}
 
