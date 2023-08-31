9ecb98a57ea97c626838d62a693a45c1d6a6a203
osparking <jbpark03@gmail.com>
2023-08-31 12:48:37 +0900

    search beans from domain package

---
+++ src/main/java/jb/ssh/ch03_ex6/config/ProjectConfig.java
@@ -0,0 +1,10 @@
+package jb.ssh.ch03_ex6.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "jb.ssh.ch03_ex6.domain")
+public class ProjectConfig {
+
+}
